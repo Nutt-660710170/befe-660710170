99217f38b83c916a28bbbe0719770555e9234f0d
Nutt-660710170 <Singsatit_N@silpakorn.edu>
2025-09-17 05:45:16 +0000

    week7-lab

---
+++ week7-lab2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func initDB() {
+	host := getEnv("DB_HOST", "")
+	name := getEnv("DB_NAME", "")
+	user := getEnv("DB_USER", "")
+	password := getEnv("DB_PASSWORD", "")
+	port := getEnv("DB_PORT", "")
+
+	conSt := fmt.Sprintf("host=%s port=%s user=%s password=%s db_name=%s sslmode=disable", host, port, user, password, name)
+	fmt.Println(conSt)
+}
+
+func main() {
+	initDB()
+}
